8eb80fa229854f4e22503b0a7851b1a0a3c09c59
lxy1010 <3329680030@qq.com>
2024-06-27 23:21:40 +0800

    class TodoList

--- Todo.py
+++ Todo.py
@@ -66,8 +66,13 @@ class TodoList:
     def __init__(self):
         self.todoList: list[Do] = []
 
-    def add_todo(self, todo: Do):
-        self.todoList.append(todo)
+    def add_todo(self, name, describe, year, month, day, hour, minute, second):
+        self.todoList.append(Do(name, describe, year, month, day, hour, minute, second))
+
+    def print_out(self):
+        for i in range(len(self.todoList)):
+            print(f"待办 {i + 1}")
+            self.todoList[i].print_out()
 
 
 class User:
